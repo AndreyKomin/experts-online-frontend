8c4de4dd1e36c1b41dae766ea13f911a3c548928
AndreyKomin <4ndrello@gmail.com>
2017-11-11 00:31:25 +0500

    Add docker-compose config

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <form action="/search" class="form">
+    <input type="text" name="tags" class="form-control" v-model="query">
+    <router-link class="btn btn-primary submit" :to="{ path: 'search', query: { tags: query }}">Поиск</router-link>
+  </form>
+</template>
+
+<script>
+
+  export default {
+    name: 'search-form',
+    data() {
+      return {
+        query: ""
+      }
+    }
+  }
+
+</script>
+
+
+<style lang="stylus" scoped>
+  .form
+    margin-top 40px
+    width 100%
+
+  .submit
+    margin-top 10px
+
+  .form-control
+    display block
+    width 100%
+    padding .375rem .75rem
+    font-size 1rem
+    line-height 1.5
+    color #495057
+    background-color #fff
+    background-image none
+    background-clip padding-box
+    border 1px solid #ced4da
+    border-radius .25rem
+    transition border-color ease-in-out .15s,box-shadow ease-in-out .15s
+
+  .form-control:focus
+    color #495057
+    background-color #fff
+    border-color #80bdff
+    outline 0
+    box-shadow 0 0 0 0.2rem rgba(0,123,255,.25)
+
+  .btn
+    display inline-block
+    font-weight 400
+    text-align center
+    white-space nowrap
+    vertical-align middle
+    -webkit-user-select none
+    -moz-user-select none
+    -ms-user-select none
+    user-select none
+    border 1px solid transparent
+    padding .375rem .75rem
+    font-size 1rem
+    line-height 1.5
+    border-radius .25rem
+    transition background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out
+
+  .btn-primary
+    background-color #5682a3
+    border 1px solid #5682a3
+    color #fff
+
+  .btn-primary.focus, .btn-primary:focus
+    box-shadow 0 0 0 0.2rem rgba(0,123,255,.5)
+    outline 0
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,21 @@
 
   <div class="banner">
     <h1 class="heading">Найди своего эксперта с нами!</h1>
+
+    <h3 class="desc">Популярные запросы:</h3>
     <div class="buttons">
       <router-link class="button" v-for="button in buttons" :key="button.id" :to="{ path: 'search', query: { tags: button.tag }}">{{button.name}}</router-link>
+    </div>
+    <div>
+      <SearchForm />
     </div>
   </div>
 
 </template>
 
 <script>
+
+  import SearchForm from 'components/SearchForm.vue'
 
 export default {
   name: 'home', // required
@@ -38,6 +45,9 @@ export default {
         },
       ]
     }
+  },
+  components: {
+    SearchForm
   }
 //  props: ['item'],
   // http://ssr.vuejs.org/en/caching.html#component-level-caching
@@ -63,6 +73,11 @@ export default {
     width 100%
     display flex
     justify-content center
+
+
+  .desc
+    margin-bottom 10px
+    width 100%
 
 
 </style>
