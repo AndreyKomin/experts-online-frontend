34048d6512836f1caec2ac70668a9a4f153b361d
AndreyKomin <andrey.komin@saritasa.com>
2017-12-11 11:14:38 +0500

    Api url update

--- src/api/index.js
+++ src/api/index.js
@@ -3,18 +3,20 @@ import mockData from './mockData';
 import { capitalizeFirstLetter } from './mockFunc';
 
 
+const apiUrl = (process.env.NODE_ENV !== 'production') ? `http://0.0.0.0:7000` : `http://api.эксперты-онлайн.рф`;
+
 axios.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
 
   const originalRequest = error.config;
 
-  if (error.response.status === 401 && !originalRequest._retry && error.response.request.responseURL !== 'http://0.0.0.0:7000/api/auth') {
+  if (error.response.status === 401 && !originalRequest._retry && error.response.request.responseURL !== `${apiUrl}/api/auth`) {
 
     originalRequest._retry = true;
 
     const token = window.localStorage.getItem('refreshToken');
-    return axios.put('http://0.0.0.0:7000/api/auth', { token })
+    return axios.put(`${apiUrl}/api/auth`, { token })
       .then(({data}) => {
         window.localStorage.setItem('token', data.token);
         window.localStorage.setItem('refreshToken', data.refreshToken);
@@ -47,7 +49,7 @@ export function fetchItems (ids) {
 }
 
 export function auth (login, messenger_id, code) {
-  return axios.post(`http://0.0.0.0:7000/api/auth/`, { login, messenger_id, code }).then(response => {
+  return axios.post(`${apiUrl}/api/auth/`, { login, messenger_id, code }).then(response => {
     return response.data.token
   })
 }
@@ -65,29 +67,29 @@ export function fetchToken (type) {
 export function fetchMe (token) {
   token = token || localStorage['token'];
   if (token == null) return Promise.reject();
-  return axios.get(`http://0.0.0.0:7000/api/me/`, { headers: { 'Authorization': 'Bearer ' + token } }).then(response => {
+  return axios.get(`${apiUrl}/api/me/`, { headers: { 'Authorization': 'Bearer ' + token } }).then(response => {
     return response.data.data
   })
 }
 
 export function fetchUser (id) {
-  return axios.get(`http://0.0.0.0:7000/api/users/${id}`).then(response => {
+  return axios.get(`${apiUrl}/api/users/${id}`).then(response => {
     return response.data.data
   });
 }
 
 export function fetchUsers () {
-  return axios.get(`http://0.0.0.0:7000/api/users/`).then(response => {
+  return axios.get(`${apiUrl}/api/users/`).then(response => {
     return response.data.data
   });
 }
 
 export function search (searchQuery) {
-  return axios.get(`http://0.0.0.0:7000/api/search`, { params: { q: searchQuery } }).then(response => {
+  return axios.get(`${apiUrl}/api/search`, { params: { q: searchQuery } }).then(response => {
     return response.data.data
   });
 }
 
 export function updateMe (token, data) {
-  return axios.put(`http://0.0.0.0:7000/api/me/`, { ...data }, { headers: { 'Authorization': 'Bearer ' + token } })
+  return axios.put(`${apiUrl}/api/me/`, { ...data }, { headers: { 'Authorization': 'Bearer ' + token } })
 }
